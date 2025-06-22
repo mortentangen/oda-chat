4a5df271666869e097da2c8255524900a3e749af
Morten Tangen <morten.tangen@gmail.com>
2025-06-22 22:11:12 +0200

    add GoogleProvider

--- auth.ts
+++ auth.ts
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth"
-import Google from "next-auth/providers/google"
+import GoogleProvider from "next-auth/providers/google"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
+  providers: [GoogleProvider({
+    clientId: process.env.GOOGLE_CLIENT_ID!,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+  })],
 })
